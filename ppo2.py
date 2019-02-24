747c6ae7a1ccd57c6cc2d50813e62506028eb182
gabrielbarcik <gbarcike@gmail.com>
2019-02-24 21:58:42 +0000

    using cnn policy

--- ppo2.py
+++ ppo2.py
@@ -6,17 +6,21 @@ os.environ["SDL_VIDEODRIVER"] = "dummy"
 import gym
 import gym_flappy_bird
 
-from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.policies import CnnPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines import PPO2
 
 # multiprocess environment
-n_cpu = 8
-env = SubprocVecEnv([lambda: gym.make('flappy-bird-v0') for i in range(n_cpu)])
+# n_cpu = 16
+# env = SubprocVecEnv([lambda: gym.make('flappy-bird-v0') for i in range(n_cpu)])
 
-model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='./tmp/flappy_bird25000/')
-model.learn(total_timesteps=1000000)
-model.save("ppo2_flappy_bird")
+env = gym.make('flappy-bird-v0')
+env = DummyVecEnv([lambda: env])
+
+model = PPO2(CnnPolicy, env, verbose=1, tensorboard_log='./tmp/flappy_bird_cnn/')
+model.learn(total_timesteps=3000000)
+model.save("ppo2_flappy_bird_cnn")
 
 print('Finished')
 
