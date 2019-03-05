590c06312b49a68bf03142ed828cda8eb1a0fbc7
gabrielbarcik <gbarcike@gmail.com>
2019-03-05 15:38:24 +0000

    fix using env wrapper

--- ppo2.py
+++ ppo2.py
@@ -6,21 +6,32 @@ os.environ["SDL_VIDEODRIVER"] = "dummy"
 import gym
 import gym_flappy_bird
 
+from stable_baselines.common.cmd_util import make_atari_env
 from stable_baselines.common.policies import CnnPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines.common.vec_env import VecFrameStack
 from stable_baselines import PPO2
+
+from env_wrapper import make_flappy_env
+
 
 # multiprocess environment
 # n_cpu = 16
 # env = SubprocVecEnv([lambda: gym.make('flappy-bird-v0') for i in range(n_cpu)])
 
-env = gym.make('flappy-bird-v0')
-env = DummyVecEnv([lambda: env])
+# env = gym.make('flappy-bird-v0')
+# env = DummyVecEnv([lambda: env])
 
-model = PPO2(CnnPolicy, env, verbose=1, tensorboard_log='./tmp/flappy_bird_cnn/')
-model.learn(total_timesteps=3000000)
-model.save("ppo2_flappy_bird_cnn")
+ENV_ID = 'flappy-bird-v0'
+
+env = make_flappy_env(ENV_ID, num_env=16, seed=0)
+# Frame-stacking with 4 frames
+env = VecFrameStack(env, n_stack=4)
+
+model = PPO2(CnnPolicy, env, verbose=1, tensorboard_log='./tmp/flappy_bird_cnn_test/')
+model.learn(total_timesteps=10000)
+model.save("ppo2_flappy_bird_cnn_test")
 
 print('Finished')
 
