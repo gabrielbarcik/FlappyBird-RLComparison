0bb287e8b6e06b575a2b3199cddbb413f7d57c2f
viniciusoliveirasd <viniciusoliveirasd@gmail.com>
2019-03-06 15:55:04 +0000

    snnPolicy added to acer.py script

--- acer.py
+++ acer.py
@@ -7,7 +7,7 @@ import gym
 import gym_flappy_bird
 import datetime
 
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy
+from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy, CnnPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.vec_env import VecFrameStack
 
@@ -20,15 +20,14 @@ env = make_flappy_env(ENV_ID, num_env=1, seed=0)
 # Frame-stacking with 4 frames
 env = VecFrameStack(env, n_stack=4)
 
-model = ACER(MlpLstmPolicy, env, verbose=1, tensorboard_log='./acer/flappy_bird_test_1000/')
-
+model = ACER(CnnPolicy, env, verbose=1, tensorboard_log='./acer/acer_1kk_cnn/')
 
 start_time = datetime.datetime.now()
 
-model.learn(total_timesteps=1000)
+model.learn(total_timesteps=1000000)
 
 print(datetime.datetime.now() - start_time)
-model.save("acer_1k_test")
+model.save("acer_1kk_cnn")
 
 print('Finished')
 
